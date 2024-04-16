ed7fbb9674719e5a3efa28bc9288fb9b9ce548ef
Wolfgang Prett <wolfgang.prett@topinform.info>
2024-04-16 14:28:47 +0200

    bullshit einegschriebn

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
 
 
+    public static void main(String[] args){
+
+        //1.Par in
+        String[] args = ReadParameters();
+        //2. Val par
+        validateParameters(args);
+
+        /
+
+    }
+
+
 
 }
