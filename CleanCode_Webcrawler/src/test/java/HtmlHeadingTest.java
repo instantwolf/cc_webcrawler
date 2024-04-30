48efcaad0e2789d015f79c56b68a39b70280e4c8
Elias Wassertheurer <eliaswa@edu.aau.at>
2024-04-30 14:34:20 +0200

    Added Test Cases for HtmlHeading.java

---
+++ CleanCode_Webcrawler/src/test/java/HtmlHeadingTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class HtmlHeadingTest {
+    private HtmlHeading heading;
+
+    @Test
+    public void createHeadingWithTagTest() {
+        heading = new HtmlHeading("h1", "Heading 1");
+        assertNotNull(heading);
+    }
+
+    @Test
+    public void createHeadingWithHtmlHeadingLevelTest() {
+        heading = new HtmlHeading(HtmlHeadingLevel.H1, "Heading 1");
+        assertNotNull(heading);
+    }
+
+    @Test
+    public void createHeadingWithHeadingLevelTest() {
+        heading = new HtmlHeading(1, "Heading 1");
+        assertNotNull(heading);
+    }
+
+    @Test
+    public void getContentTest() {
+        heading = new HtmlHeading(1, "Heading 1");
+        assertEquals("Heading 1", heading.getContent());
+    }
+
+    @Test
+    public void setContentTest() {
+        heading = new HtmlHeading(1, "");
+        heading.setContent("Heading 1");
+        assertEquals("Heading 1", heading.getContent());
+    }
+
+    @Test
+    public void getHeadingLevelTest() {
+        heading = new HtmlHeading("h1", "");
+        assertEquals(1, heading.getHeadingLevelInt());
+    }
+
+}
