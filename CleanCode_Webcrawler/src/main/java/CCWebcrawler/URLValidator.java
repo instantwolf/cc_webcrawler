a6925a45afc094e268279e968091c1982c462231
Elias Wassertheurer <eliaswa@edu.aau.at>
2024-06-10 22:06:07 +0200

    Regex updated

--- CleanCode_Webcrawler/src/main/java/CCWebcrawler/URLValidator.java
+++ CleanCode_Webcrawler/src/main/java/CCWebcrawler/URLValidator.java
@@ -8,7 +8,8 @@ import java.util.regex.Pattern;
 public class URLValidator {
 
     private static final String URL_REGEX =
-            "^(http|https)://[a-zA-Z0-9]+([\\-\\.]{1}[a-zA-Z0-9]+)\\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\\/\\S)?$";
+            "^((http|https):\\/\\/)?[a-zA-Z0-9]+(([\\-\\.])?[a-zA-Z0-9]*)\\.[a-zA-Z]{2,5}(\\/\\w*)*$";
+            //"^(http|https)://[a-zA-Z0-9]+([\\-\\.]{1}[a-zA-Z0-9]+)\\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\\/\\S)?$";
 
     private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
