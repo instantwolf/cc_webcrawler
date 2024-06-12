55b5cbdfddaa8fe2c2cfe6ce08a0eb630a7584a6
Wolfgang Prett <wolfgang.prett@topinform.info>
2024-06-12 12:11:34 +0200

    refarctored input texts, removed unused methods and properties in URLValidator

--- CleanCode_Webcrawler/src/main/java/CCWebcrawler/InputHandler/Console/ConsoleInputHandlerTexts.java
+++ CleanCode_Webcrawler/src/main/java/CCWebcrawler/InputHandler/Console/ConsoleInputHandlerTexts.java
@@ -2,13 +2,16 @@ package CCWebcrawler.InputHandler.Console;
 
 public enum ConsoleInputHandlerTexts {
 
-    URL_PROMPT("Enter the desired URL like: https://www.example.com "),
-    INVALID_URL_RESPONSE("Invalid URL. Please provide a valid URL: "),
+    URL_PROMPT("Enter the desired URL like: https://www.example.com. \n" +
+            "If you want to crawl multiple pages in parallel, enter the URLs in the given format separated by a whitespace. \n" +
+            "eg. [URL1] [URL2]"),
+    INVALID_URL_RESPONSE("Invalid URL. Please provide a valid URL: \n " +
+            "URL Pattern: ((http|https)://)?(www.)?([subdomain].)?[domain].[top-level-domain](/[URI])?"),
     TARGET_DEPTH_PROMPT("Enter the depth of the websites to crawl: "),
     INVALID_TARGET_DEPTH_PROMPT("Invalid depth, please provide a positive  number bigger than zero"),
     ERROR_READING_INPUT("Invalid depth, please provide a positive  number bigger than zero");
 
-    private String text;
+    private final String text;
 
     ConsoleInputHandlerTexts(String text){
         this.text = text;
--- CleanCode_Webcrawler/src/main/java/CCWebcrawler/URLValidator.java
+++ CleanCode_Webcrawler/src/main/java/CCWebcrawler/URLValidator.java
@@ -7,8 +7,6 @@ import java.util.regex.Pattern;
 
 public class URLValidator {
 
-    private static String protocol = "https://";
-
     private static final String URL_REGEX =
             "^((http|https):\\/\\/)?[a-zA-Z0-9]+(([\\-\\.])?[a-zA-Z0-9]*)\\.[a-zA-Z]{2,5}(\\/\\w*)*$";
             //"^(http|https)://[a-zA-Z0-9]+([\\-\\.]{1}[a-zA-Z0-9]+)\\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\\/\\S)?$";
@@ -28,10 +26,5 @@ public class URLValidator {
     public static boolean hasProtocol(String url) {
         Matcher matcher = HASPROTOCOL_PATTERN.matcher(url);
         return matcher.matches();
-    }
-
-    public static String addProtocol(String url) {
-        url = protocol + url;
-        return url;
     }
 }
